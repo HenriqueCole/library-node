98c0fbe699dcb7a9024cb798284c937fa187754a
HenriqueCole <henriquecolefernandes@gmail.com>
2022-07-18 21:39:18 -0300

    just need some adjustments

--- api/author/authors.controller.js
+++ api/author/authors.controller.js
@@ -1,18 +1,61 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const authorsHandler = require('./authors.handler');
+const authorsHandler = require("./authors.handler");
 
-router.post('/authors', async (req, res) =>{
-    authorsHandler.addAuthors(req.body)
-    .then(result => {
-        res.status(200).json(result);
-    }
-    ).catch(err => {
-        res.status(500).json(err);
-    }
-    );
-})
+router.post("/authors", async (req, res) => {
+  authorsHandler
+    .addAuthors(req.body)
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
 
+router.get("/authors", async (req, res) => {
+  authorsHandler
+    .getAuthors()
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
+router.get("/authors/:id", async (req, res) => {
+  authorsHandler
+    .getAuthorById(req.params.id)
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.delete("/authors/:id", async (req, res) => {
+  authorsHandler
+    .deleteAuthor(req.params.id)
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
+router.put("/authors/:id", async (req, res) => {
+  authorsHandler
+    .updateAuthor(req.params.id, req.body)
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
 
 module.exports = router;
--- api/book/books.handler.js
+++ api/book/books.handler.js
@@ -1,12 +1,32 @@
 const crud = require("../../crud");
 
-async function addBooks(books) {
+const authorHandler = require("../author/authors.handler");
+
+async function addBooks(data) {
+  const authors = await authorHandler.getAuthors();
+
   const book = {
-    title: books.title,
-    genre: books.genre,
-    year: books.year,
+    title: data.title,
+    genre: data.genre,
+    year: data.year,
     status: "available",
+    authorsList: data.authorsList,
   };
+
+  if (Array.isArray(data.authorsList)) {
+    data.authorsList.forEach((authorId) => {
+      const author = authors.find((author) => author.id === authorId);
+      if (author) {
+        book.authorsList.push(author);
+      } else {
+        throw {
+          Error: "You can't add a book because the author doesn't exist.",
+        };
+      }
+    }
+    );
+  }
+  
   const savedBook = await crud.post("books", null, book);
   return savedBook;
 }
